5f4194e29351bbd2cca03183704358130afc178b
VAV <menghsin925@gmail.com>
2020-05-27 14:12:41 +0800

    first commit

---
+++ mydatas.js
@@ -0,0 +1,56 @@
+const myExp=[
+    {"time":"2010年6月", "text":"畢業於 台中女子高級中學", "type":"edu"},
+    {"time":"2013年8月", "text":"主辦第一場販售會活動", "type":"event"},
+    {"time":"2014年6月", "text":"畢業於 台藝大視覺傳達設計系", "type":"edu"},
+    {"time":"2014年9月", "text":"於 冉色斯動畫 任2D美術設計", "type":"work"},
+    {"time":"2016年2月", "text":"主辦第二場販售會活動", "type":"event"},
+    {"time":"2016年3月", "text":"於 艾法諾科技 任資深美術設計", "type":"work"},
+    {"time":"2018年9月", "text":"於 日本 就讀語文學校", "type":"edu"},
+    {"time":"2020年5月", "text":"結業於 資策會-前端養成班", "type":"edu"}
+]
+
+
+
+const imgData = [
+    {"num":"001", "src":"img/illust/001.png", "type":"illust", "date": 20190731},
+    {"num":"002", "src":"img/illust/002.png", "type":"illust", "date": 20151231},
+    {"num":"003", "src":"img/illust/003.png", "type":"illust", "date": 20190415},
+    {"num":"004", "src":"img/illust/004.jpg", "type":"illust", "date": 20150226},
+    {"num":"005", "src":"img/illust/005.png", "type":"illust", "date": 20170101},
+    {"num":"006", "src":"img/illust/006.png", "type":"illust", "date": 20180801},
+    {"num":"007", "src":"img/illust/007.png", "type":"illust", "date": 20160612},
+    {"num":"008", "src":"img/illust/008.png", "type":"illust", "date": 20171209},
+    {"num":"009", "src":"img/cute/009.jpg", "type":"cute", "date": 20151202},
+    {"num":"010", "src":"img/cute/010.png", "type":"cute", "date": 20191202},
+    {"num":"011", "src":"img/cute/011.png", "type":"cute", "date": 20161024},
+    {"num":"012", "src":"img/charactor/012.png", "type":"charactor", "date": 20200116},
+    {"num":"013", "src":"img/charactor/013.png", "type":"charactor", "date": 20200116},
+    {"num":"014", "src":"img/background/014.png", "type":"background", "date": 20161016},
+    {"num":"015", "src":"img/background/015.png", "type":"background", "date": 20170216},
+    {"num":"016", "src":"img/illust/016.png", "type":"illust", "date": 20190710},
+    {"num":"017", "src":"img/illust/017.png", "type":"illust", "date": 20190518},
+    {"num":"018", "src":"img/illust/018.png", "type":"illust", "date": 20200308},
+    {"num":"019", "src":"img/illust/019.png", "type":"illust", "date": 20180427},
+    {"num":"020", "src":"img/cute/020.jpg", "type":"cute", "date": 20151102},
+    {"num":"021", "src":"img/cute/021.jpg", "type":"cute", "date": 20151103},
+    {"num":"022", "src":"img/cute/022.jpg", "type":"cute", "date": 20151104},
+    {"num":"023", "src":"img/cute/023.png", "type":"cute", "date": 20180422},
+    {"num":"024", "src":"img/illust/024.png", "type":"illust", "date": 20190720},
+    {"num":"025", "src":"img/background/025.png", "type":"background", "date": 20170216},
+    {"num":"026", "src":"img/charactor/026.png", "type":"charactor", "date": 20160717},
+    {"num":"027", "src":"img/charactor/027.png", "type":"charactor", "date": 20160718},
+    {"num":"028", "src":"img/charactor/028.png", "type":"charactor", "date": 20160719},
+    {"num":"029", "src":"img/charactor/029.png", "type":"charactor", "date": 20160720},
+    {"num":"030", "src":"img/illust/030.png", "type":"illust", "date": 20161221},
+    {"num":"031", "src":"img/illust/031.png", "type":"illust", "date": 20170307},
+    {"num":"032", "src":"img/illust/032.png", "type":"illust", "date": 20190615},
+    {"num":"033", "src":"img/illust/033.png", "type":"illust", "date": 20190716},
+    {"num":"034", "src":"img/charactor/034.png", "type":"charactor", "date": 20160721},
+    {"num":"035", "src":"img/charactor/035.png", "type":"charactor", "date": 20160722},
+    {"num":"036", "src":"img/charactor/036.png", "type":"charactor", "date": 20160723},
+    {"num":"037", "src":"img/illust/037.jpg", "type":"illust", "date": 20150226},
+    {"num":"038", "src":"img/illust/038.jpg", "type":"illust", "date": 20140610},
+    {"num":"039", "src":"img/illust/039.png", "type":"illust", "date": 20180821},
+    {"num":"040", "src":"img/charactor/040.png", "type":"charactor", "date": 20200116}
+]
+
---
+++ profile.js
@@ -0,0 +1,214 @@
+//---------------------------- first part animation ------------------------------
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.titleanime .mytitle');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.titleanime .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  });
+
+let domilk = document.querySelector('#domilk'); 
+setInterval(function() {
+    domilk.style.display = 'block';
+}, 800);
+
+
+let firsthieght = document.querySelector("#con-first").offsetHeight;
+let nexthieght = document.querySelector(".bg-white-after").offsetHeight;
+let lasthieght = firsthieght + nexthieght;
+$('#scroll-next').on('click', () => window.scrollTo({
+    top: lasthieght,
+    behavior: 'smooth',
+  }));
+
+let scrollnext = document.querySelector('.first-button-part'); 
+setInterval(function() {
+    scrollnext.style.display = 'block';
+}, 2500);
+
+//---------------------------- second part profile ------------------------------
+
+
+function ExpContext(){
+    let expHTML = ''
+    for (let i = 0; i < myExp.length; i++) {
+        expHTML += 
+            `<div class="exp-img">
+                <div class="exp-img-first" data-expstart=${i}></div>
+                <div class="exp-img-second exp-img-${myExp[i].type}"></div>
+                <div class="exp-img-third" data-expend=${i}></div>
+            </div>
+            <div class="exp-text">
+                <div class="exp-text-first">
+                </div>
+                <div class="exp-text-second">
+                <span class="exp-text-time"><b>${myExp[i].time}</b></span><span>${myExp[i].text}</span>
+                </div>
+                <div class="exp-text-third"></div>
+            </div>`;
+    }
+    return expHTML
+}
+
+let profileExp = document.querySelector("#profile-exp") ;
+profileExp.innerHTML = ExpContext();
+
+let expImgFirst = document.querySelectorAll(".exp-img-first");
+let expImgEnd = document.querySelectorAll(".exp-img-third");
+expImgFirst.forEach((e) => {
+    if(e.dataset.expstart==0){
+        e.classList.add("exp-img-none")
+    }})
+    expImgEnd.forEach((e) => {
+    if(e.dataset.expend==7){
+        e.classList.add("exp-img-none")
+    }})
+
+
+
+//---------------------------- forth part pictures ------------------------------
+// ----------------------------- get array, output html -----------------------------
+function PicContext(myArr){
+    let picHTML = ''
+    for (let i = 0; i < myArr.length; i++) {
+        picHTML += 
+            `<div class='picture pic-${myArr[i].type}'
+                         data-id='${myArr[i].num}'>
+                <img src='${myArr[i].src}' class='img-fluid'>
+            </div>`;
+    }
+    return picHTML
+}
+
+function UpdateDiv(myHtml) {
+    let picInner = document.querySelector(".picInner");
+    picInner.innerHTML = myHtml;
+    const pictures = document.querySelectorAll(".picture");
+    pictures.forEach(e => e.addEventListener('click', discPic));
+
+}
+
+//--------------------- print all works on page load -----------------------------
+
+const allWorkHtml = PicContext(imgData);
+UpdateDiv(allWorkHtml);
+
+//-----------------------------  picture sort ----------------------------------
+
+function sortOrigin(){
+    const newArr = imgData.sort(function (a, b) {
+        return Number(a.num) - Number(b.num);
+      });
+      return newArr;
+}
+
+function sortOld(){
+    const newArr = imgData.sort(function (a, b) {
+        return a.date - b.date;
+      });
+      return newArr;
+}
+
+
+function sortNew(){
+    const newArr = imgData.sort(function(a, b) {
+        if (a.date > b.date) return -1;
+        if (a.date < b.date) return 1;
+        return 0;
+    });
+    return newArr;
+}
+
+function letsortOrigin(){
+    const sortedArray = sortOrigin(); // 新的陣列
+    let newHtml = PicContext(sortedArray); // 新的陣列轉乘的ＨＴＭＬ字串
+    UpdateDiv(newHtml); //更新DIV
+}
+
+function clickNewBtn(){
+    const sortedArray = sortNew(); // 新的陣列
+    let newHtml = PicContext(sortedArray); // 新的陣列轉乘的ＨＴＭＬ字串
+    UpdateDiv(newHtml); //更新DIV
+}
+
+function clickOldBtn(){
+    const sortedArray = sortOld();
+    let newHtml = PicContext(sortedArray);
+    UpdateDiv(newHtml); //更新DIV
+    
+}
+
+//-----------------------------  modal & clickpicture ----------------------------------
+
+const modal = document.querySelector("#myModal");
+const modalPic = document.querySelector(".modal-pic");
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+function discPic() {
+    modal.style.display = "block";
+    let lastNum = this.dataset.id;
+    const foundWork = imgData.find(function(item){
+        return item.num === lastNum;           // 取得大於五歲的
+      });
+      
+    let context = "<img src=" + foundWork.src + " class='img-fluid'>";
+    modalPic.innerHTML = context;
+}
+
+
+//-----------------------------  picture selector ----------------------------------
+const picInner = document.querySelector("#pic-inner")
+const picBtns = document.querySelectorAll(".pic-buttons button")
+picBtns.forEach(e=> e.addEventListener('click',picSelector))
+
+
+// filter
+function picSelector(){
+    switch(this.name){
+        case "all" : 
+            picInner.classList.remove("is-show-illust", "is-show-cute", "is-show-charactor", "is-show-background");
+            letsortOrigin();
+            break;
+        case "illust" : 
+            picInner.classList.toggle("is-show-illust");
+            this.classList.toggle("my-btn-toggle");
+            break;
+        case "cute" : 
+            picInner.classList.toggle("is-show-cute");
+            this.classList.toggle("my-btn-toggle");
+            break;
+        case "charactor" : 
+            picInner.classList.toggle("is-show-charactor"); 
+            this.classList.toggle("my-btn-toggle");
+            break;
+        case "background" : 
+            picInner.classList.toggle("is-show-background"); 
+            this.classList.toggle("my-btn-toggle");
+            break;
+    }
+
+}
+
+//------------------------- scroll top --------------------------------
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function(){$(".scroll").click(function(){window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })})})

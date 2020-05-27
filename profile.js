de572b9effd5f75da4ee599f411cbebb9c557782
vav925 <59193104+vav925@users.noreply.github.com>
2020-05-27 15:59:03 +0800

    update window onload

--- profile.js
+++ profile.js
@@ -1,3 +1,8 @@
+window.onload(function() {
+    $('#body').show();
+    $('#msg').hide();
+});
+
 //---------------------------- first part animation ------------------------------
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.titleanime .mytitle');
